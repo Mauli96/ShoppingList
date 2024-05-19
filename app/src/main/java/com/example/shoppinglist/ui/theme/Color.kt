0b04d2ea8d74afcfccc577ad3ae772fe6a27b14c
Mauli Waghmore <mauliwaghmore9696@gmail.com>
2024-05-19 12:09:50 +0530

    Update Color.kt

--- app/src/main/java/com/example/shoppinglist/ui/theme/Color.kt
+++ app/src/main/java/com/example/shoppinglist/ui/theme/Color.kt
@@ -2,7 +2,7 @@ package com.example.shoppinglist.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
+val Purple80 = Color(0xFF3E11E2)
 val PurpleGrey80 = Color(0xFFCCC2DC)
 val Pink80 = Color(0xFFEFB8C8)
 
